fc482b993fd604b7f22cdf97f5659717a56487a5
Vinayak-Gangrade <gangradevinayak001@gmail.com>
2026-03-10 14:45:09 +0530

    UC 1 - Application Entry & Welcome Message

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,19 @@
+
+public class BookMyStayApp {
+
+    /**
+     * Main method - entry point of the Java application
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("Welcome to Hotel Booking System");
+
+        // Display application name and version
+        System.out.println("Application Name: Book My Stay");
+        System.out.println("Version: 1.0");
+
+        // End message
+        System.out.println("Application Started Successfully");
+    }
+}
